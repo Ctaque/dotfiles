9a6ea246e1125044994e54a4e2268249de8b0115
Cyprien Taque <cyprien.taque@gmail.com>
2020-02-17 22:26:07 +0100

    add xmonad conf

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,27 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+import XMonad.Config.Desktop
+import XMonad.Wallpaper
 
-main = xmonad =<< xmobar defaultConfig { modMask = mod4Mask }
+main = do
+       xmproc <- spawnPipe "xmobar"
+       xmproc <- spawnPipe "xscreensaver -nosplash"
+
+       setRandomWallpaper ["$HOME/Images"]
+       xmonad $ defaultConfig { manageHook = manageDocks <+> manageHook defaultConfig
+                              , layoutHook = avoidStruts $ layoutHook defaultConfig
+                              , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
+                              , terminal = myTerminal
+                              , normalBorderColor = myNormalBorderColor
+                              , focusedBorderColor = myFocusedBorderColor
+                              , borderWidth = myBorderWidth
+                              }
+
+myTerminal = "urxvt"
+myBorderWidth = 2
+myNormalBorderColor = "black"
+myFocusedBorderColor = "red"
